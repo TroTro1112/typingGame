778234abcd736ab30d67ba3fc01b25890e3a2e32
NomuraRen <kd1250242@st.kobedenshi.ac.jp>
2022-08-13 15:35:51 +0900

    カウントタイマーを作る

---
+++ Typing/Assets/Scripts/System/Timer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float m_time;
+    [SerializeField]
+    private Text m_timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_time = 30.0f;
+        m_timerText.text = "0.00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ê›íËâÊñ Ç≈30ïbÇæÇ¡ÇΩÇÁ
+        //if()
+
+        //ê›íËâÊñ Ç≈60ïbÇæÇ¡ÇΩÇÁ
+        //if()
+
+        m_time -= Time.deltaTime;
+        m_timerText.text = m_time.ToString("0.00");
+
+        if (m_time < 0) 
+        {
+
+        }
+    }
+}
