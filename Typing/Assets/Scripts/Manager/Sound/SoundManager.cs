6b36a664ee99e91431754a4d9ad5be492f631de9
TroTro1112 <cguguruguru1112@gmail.com>
2022-10-15 13:40:12 +0900

    ゲームシーンBGM

--- Typing/Assets/Scripts/Manager/Sound/SoundManager.cs
+++ Typing/Assets/Scripts/Manager/Sound/SoundManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
@@ -30,21 +31,37 @@ public class SoundManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        // スライダー
-        m_Slider_BGM = m_obj_BGM.GetComponent<Slider>();
-        m_Slider_SE = m_obj_SE.GetComponent<Slider>();
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Title":
+                // volume
+                m_Slider_BGM = m_obj_BGM.GetComponent<Slider>();
+                m_Slider_SE = m_obj_SE.GetComponent<Slider>();
 
-        float max = 100f;
-        float now = 100f;
-        //スライダーの最大値・最小値の設定
-        m_Slider_BGM.maxValue = max;
-        m_Slider_BGM.minValue = 0f;
-        m_Slider_SE.maxValue = max;
-        m_Slider_SE.minValue = 0f;
+                float max = 100f;
+                float now = 100f;
+                //スライダーの最大値・最小値の設定
+                m_Slider_BGM.maxValue = max;
+                m_Slider_BGM.minValue = 0f;
+                m_Slider_SE.maxValue = max;
+                m_Slider_SE.minValue = 0f;
 
-        //スライダーの現在値の設定
-        m_Slider_BGM.value = now;
-        m_Slider_SE.value = now;
+                //スライダーの現在値の設定
+                m_Slider_BGM.value = now;
+                m_Slider_SE.value = now;
+                break;
+            case "Game":
+                // データが保存されている
+                if (PlayerPrefs.HasKey("Value_BGM"))
+                {
+                    m_AudioSource_BGM.volume = (PlayerPrefs.GetFloat("Value_BGM") / 100.0f);
+                }
+                else
+                {
+                    m_AudioSource_BGM.volume = 0.3f;
+                }
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -53,13 +70,16 @@ public class SoundManager : MonoBehaviour
         // ミュート
         if (m_IsMute == true) return;
 
-        m_AudioSource_BGM.volume = (m_Slider_BGM.value / 100.0f);
-        m_AudioSource_SE.volume = (m_Slider_SE.value / 100.0f);
-
-        // 音量設定を保存
-        PlayerPrefs.SetFloat("Value_BGM", m_Slider_BGM.value);
-        PlayerPrefs.SetFloat("Value_SE", m_Slider_SE.value);
-
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Title":
+                m_AudioSource_BGM.volume = (m_Slider_BGM.value / 100.0f);
+                m_AudioSource_SE.volume = (m_Slider_SE.value / 100.0f);
+                // 音量設定を保存
+                PlayerPrefs.SetFloat("Value_BGM", m_Slider_BGM.value);
+                PlayerPrefs.SetFloat("Value_SE", m_Slider_SE.value);
+                break;
+        }
         // クリックSE
         if (Input.GetMouseButton(0))
         {
